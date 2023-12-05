68c2b7a191df6f09c2fed61085708456092ec958
sadafa asfafas <patrisialorenaschone@gmail.com>
2023-12-05 10:25:49 +0100

    changed twitter icon

--- src/components/blueSection.jsx
+++ src/components/blueSection.jsx
@@ -7,9 +7,20 @@ import { ThemeContext } from "../App.jsx"
 
 export default function BlueSection(){
     const {lightMode} = React.useContext(ThemeContext)
+    const [isScrolled, setIsScrolled] = React.useState(false)
+
+    function changeOnScroll(){
+    if(window.scrollY >= 500){
+        setIsScrolled(true)
+    }else{
+        setIsScrolled(false)
+    }
+    }
+    window.addEventListener("scroll", changeOnScroll)
+
     return(
         <div>
-            <img src={stiker} height={150} className="graduate" alt="Patricia Lorena Stiker wearing a graduation hat" />
+            <img src={stiker} height={150} className={isScrolled ? "graduate" : "graduate"} alt="Patricia Lorena Stiker wearing a graduation hat" />
             <div className={lightMode ? "my-green-section" : "my-green-section my-green-sectionDark"}>
             <h2 id="certificates"><span>🏆 </span> Certificates</h2>
             <CerificateCard lightMode={lightMode}/>
